d3e82acb56281f20070344a16c76f9f62052293a
amebrahimi <amirebrahimi5@live.com>
2018-10-07 16:51:27 +0330

    End of section 6

---
+++ src/components/contacts/AddContact.js
@@ -0,0 +1,109 @@
+import React, {Component} from 'react';
+import {Consumer} from "../../context";
+import TextInputGroup from "../layout/TextInputGroup";
+
+import uuid from "uuid";
+
+
+class AddContact extends Component {
+    state = {
+        name: '',
+        email: '',
+        phone: '',
+        errors: {}
+    };
+
+    onInputChange = e => this.setState({[e.target.name]: e.target.value});
+    onSubmit = (dispatch, e) => {
+        e.preventDefault();
+
+        const {name, email, phone} = this.state;
+
+        // Check for Errors
+        if (name === '') {
+            this.setState({
+                errors: {
+                    name: 'Name is required'
+                }
+            });
+            return;
+        }
+        if (email === '') {
+            this.setState({
+                errors: {
+                    email: 'Email is required'
+                }
+            });
+            return;
+        }
+        if (phone === '') {
+            this.setState({
+                errors: {
+                    phone: 'Email is required'
+                }
+            });
+            return;
+        }
+
+        const newContact = {
+            id: uuid(),
+            name,
+            email,
+            phone,
+        };
+
+        dispatch({type: 'ADD_CONTACT', payload: newContact});
+
+        // Clear State
+        this.setState({
+            name: '',
+            email: '',
+            phone: '',
+            errors: {}
+        });
+        
+        this.props.history.push('/');
+    };
+
+    render() {
+        const {name, email, phone, errors} = this.state;
+
+        return (
+            <Consumer>
+                {value => {
+                    const {dispatch} = value;
+                    return (
+                        <div>
+                            <div className="card mb-3">
+                                <div className="card-header">
+                                    Add Contact
+                                </div>
+                                <div className="card-body">
+                                    <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                                        <TextInputGroup label="Name" name="name" placeholder="Enter Name.."
+                                                        value={name} onChange={this.onInputChange}
+                                                        error={errors.name}
+                                        />
+                                        <TextInputGroup label="Email" name="email" placeholder="Enter Email.."
+                                                        value={email} onChange={this.onInputChange} type="email"
+                                                        error={errors.email}
+                                        />
+                                        <TextInputGroup label="Phone" name="phone" placeholder="Enter Phone.."
+                                                        value={phone} onChange={this.onInputChange}
+                                                        error={errors.phone}
+                                        />
+                                        <input type="submit" value="Add Contact" className="btn btn-light btn-block"
+                                        />
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }}
+            </Consumer>
+        );
+
+    }
+}
+
+export default AddContact;
