f940b5e996ec5da74a6fe7e668c431ead55252db
amebrahimi <amirebrahimi5@live.com>
2018-10-07 18:40:59 +0330

    Updates

---
+++ precache-manifest.795c951e5ba1a404515ed33a50322058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "786189f398b98841d35d",
+    "url": "/react-js-practicing/static/js/runtime~main.786189f3.js"
+  },
+  {
+    "revision": "b7b79fe18632c3ac5eb4",
+    "url": "/react-js-practicing/static/js/main.b7b79fe1.chunk.js"
+  },
+  {
+    "revision": "9f94f5bc34c2b097e025",
+    "url": "/react-js-practicing/static/js/1.9f94f5bc.chunk.js"
+  },
+  {
+    "revision": "b7b79fe18632c3ac5eb4",
+    "url": "/react-js-practicing/static/css/main.4fdeda7a.chunk.css"
+  },
+  {
+    "revision": "9f94f5bc34c2b097e025",
+    "url": "/react-js-practicing/static/css/1.fc9fcba2.chunk.css"
+  },
+  {
+    "revision": "87bfdb781f7f0a495822bd1c22d9ef77",
+    "url": "/react-js-practicing/index.html"
+  }
+];
